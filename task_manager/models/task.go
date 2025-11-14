6b680f253e9b90bc6ed548e5ebf1f61535055d03
Muluken Zewge <mulukenzewge@gmail.com>
2025-11-14 14:25:38 +0300

    modify Task struct to take mongoDB id

--- task_manager/models/task.go
+++ task_manager/models/task.go
@@ -1,11 +1,18 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Task struct {
-	ID          string    `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	DueDate     time.Time `json:"due_date"`
-	Status      string    `json:"status"`
+	// MongoDB's navtive _id(internal, private field)
+	id primitive.ObjectID `bson:"_id,omitempty" json:"-"` // igonre it in the json
+	// custom id for task identification
+	ID          string    `json:"id" bson:"task_id"`
+	Title       string    `json:"title" bson:"title"`
+	Description string    `json:"description" bson:"description"`
+	DueDate     time.Time `json:"due_date" bson:"due_date"`
+	Status      string    `json:"status" bson:"status"`
 }

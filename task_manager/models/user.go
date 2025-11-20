1b029f54ae4ab8cf14e6a0ba7d82f9c3d85a0490
Muluken Zewge <mulukenzewge@gmail.com>
2025-11-20 10:20:13 +0300

    define user struct

---
+++ task_manager/models/user.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// define user role enum
+type UserRole int
+
+const (
+	RoleUser UserRole = iota
+	RoleAdmin
+)
+
+// define the user struct
+type User struct {
+	mongoID      primitive.ObjectID `bson:"_id,omitempty" json:"-"`
+	ID           uuid.UUID          `bson:"user_id" json:"id"`
+	UserName     string             `bson:"user_name" json:"user_name"`
+	PasswordHash string             `bson:"password_hash" json:"-"`
+	Role         UserRole           `bson:"role" josn:"role"`
+}

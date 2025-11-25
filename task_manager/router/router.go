2edd0e405b0573b5aa4456a51538ec14f0fc4cf9
Muluken Zewge <mulukenzewge@gmail.com>
2025-11-25 10:44:08 +0300

    modify router code for authentication and autherization

--- task_manager/router/router.go
+++ task_manager/router/router.go
@@ -1,15 +1,19 @@
 package router
 
 import (
+	"os"
 	"taskmanager/controllers"
 	"taskmanager/data"
+	"taskmanager/middleware"
+	"taskmanager/models"
 
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter(ts *data.TaskService) *gin.Engine {
-	// itialize task controller
+func SetupRouter(ts *data.TaskService, us *data.UserService) *gin.Engine {
+	// itialize task and user controller
 	taskController := controllers.NewTaskController(ts)
+	userController := controllers.NewUserController(us)
 
 	// intialize the router
 	router := gin.Default()
@@ -20,11 +24,28 @@ func SetupRouter(ts *data.TaskService) *gin.Engine {
 	// task routes
 	taskRoutes := api.Group("/tasks")
 
-	taskRoutes.GET("", taskController.GetTasks)
-	taskRoutes.POST("", taskController.CreatTask)
-	taskRoutes.GET("/:id", taskController.GetTaskById)
-	taskRoutes.PUT("/:id", taskController.UpdateTask)
-	taskRoutes.DELETE("/:id", taskController.DeleteTask)
+	// user routes
+	userRoutes := api.Group("/user")
+
+	// read jwt secret from env varaiable
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	// routes only need authentication
+	taskRoutes.GET("", middleware.AuthMiddleware(jwtSecret), taskController.GetTasks)
+	taskRoutes.GET("/:id", middleware.AuthMiddleware(jwtSecret), taskController.GetTaskById)
+
+	// admin-only routes
+	adminTaskRoutes := taskRoutes.Group("/")
+
+	adminTaskRoutes.Use(middleware.AuthMiddleware(jwtSecret))
+	adminTaskRoutes.Use(middleware.AuthorizationMiddleware(models.RoleAdmin))
+
+	adminTaskRoutes.POST("", taskController.CreatTask)
+	adminTaskRoutes.PUT("/:id", taskController.UpdateTask)
+	adminTaskRoutes.DELETE("/:id", taskController.DeleteTask)
+
+	userRoutes.POST("/register", userController.RegisterUser)
+	userRoutes.POST("/login", userController.AuthenticateUser)
 
 	return router
 }
